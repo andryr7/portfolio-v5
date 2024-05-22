9d3f87d109d16bbaccedd4813588be1cbc6c24c6
unknown <andryratsimba@gmail.com>
2024-05-22 16:10:34 +0200

    add multiple animated viewsé

--- src/components/canvas/main/OverlayScene.tsx
+++ src/components/canvas/main/OverlayScene.tsx
@@ -1,55 +1,11 @@
-import { OrbitControls, Text } from "@react-three/drei";
-import { useThree } from "@react-three/fiber";
-import { useLenis } from "lenis/react";
-import { useEffect, useState } from "react";
+import { Text } from "@react-three/drei";
 
-export function OverlayScene() {
-  const [scrollProgress, setScrollProgress] = useState(0);
-  const { getCurrentViewport } = useThree((state) => state.viewport);
-  const [viewportDimensions, setViewportDimensions] = useState({
-    width: getCurrentViewport().width,
-    height: getCurrentViewport().height,
-  });
-
-  //Handling scroll animation
-  useLenis((state) => {
-    const newScrollProgress =
-      state.animatedScroll /
-      (state.dimensions.scrollHeight - 1.5 * state.dimensions.height);
-    setScrollProgress(Math.min(newScrollProgress, 1));
-  });
-
-  //Handling window resize
-  useEffect(() => {
-    // Define the handleResize function
-    const handleResize = () => {
-      const viewport = getCurrentViewport();
-      setViewportDimensions({ width: viewport.width, height: viewport.height });
-    };
-
-    // Add event listener for resize
-    window.addEventListener("resize", handleResize);
-
-    // Cleanup function to remove the event listener when the component unmounts
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, [getCurrentViewport]);
-
+export function OverlayScene({ name = "test" }: { name: string }) {
   return (
     <>
-      <group>
-        <Text>Hello</Text>
-      </group>
-      <group>
-        <mesh>
-          <planeGeometry
-            args={[viewportDimensions.width, viewportDimensions.height]}
-          />
-          <meshBasicMaterial color="red" />
-        </mesh>
-      </group>
-      <OrbitControls />
+      <mesh>
+        <Text>{name}</Text>
+      </mesh>
     </>
   );
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,19 +1,28 @@
 import { Link, useRoute } from "wouter";
 import styles from "./Home.module.css";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { MainView } from "@/components/canvas/main/MainView";
 import { OverlayScene } from "@/components/canvas/main/OverlayScene";
 import { View } from "@react-three/drei";
+import { useLenis } from "lenis/react";
 
 gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 export function Home() {
   const [match] = useRoute("/");
   const container = useRef(null);
+  const [scrollProgress, setScrollProgress] = useState(0);
+
+  useLenis((state) => {
+    const newScrollProgress =
+      state.animatedScroll /
+      (state.dimensions.scrollHeight - 1.5 * state.dimensions.height);
+    setScrollProgress(Math.min(newScrollProgress, 1));
+  });
 
   useGSAP(
     () => {
@@ -28,12 +37,22 @@ export function Home() {
         },
       });
 
-      gsap.to("#main-canvas", {
+      gsap.to("#hero-view", {
+        scrollTrigger: {
+          trigger: "#hero",
+          start: "top top",
+          end: "bottom bottom",
+          pin: "#hero-view",
+          // markers: true,
+        },
+      });
+
+      gsap.to("#contact-view", {
         scrollTrigger: {
           trigger: "#main",
-          start: "top bottom",
+          start: "top top",
           end: "bottom bottom",
-          pin: "#main-canvas",
+          pin: "#contact-view",
           // markers: true,
         },
       });
@@ -43,25 +62,56 @@ export function Home() {
 
   return (
     <>
+      {/* Canvas views */}
       <View
         style={{
           position: "absolute",
-          top: "-100vh",
+          top: "0vh",
           height: "100vh",
           width: "100vw",
         }}
-        id="main-canvas"
+        id="hero-view"
+        index={2}
       >
-        <OverlayScene />
+        <OverlayScene name="hero view" />
       </View>
-      <header style={{ position: "absolute", zIndex: 20, opacity: 0.5 }}>
+      <View
+        style={{
+          position: "absolute",
+          top: "100vh",
+          height: "100vh",
+          width: "100vw",
+        }}
+        id="works-view"
+        index={4}
+      >
+        <OverlayScene name="projects view" />
+      </View>
+      <View
+        style={{
+          position: "absolute",
+          top: "100vh",
+          height: "100vh",
+          width: "100vw",
+        }}
+        id="contact-view"
+        index={3}
+      >
+        <OverlayScene name="contact view" />
+      </View>
+
+      {/* HTML Content */}
+      <header
+        style={{ position: "absolute", zIndex: 20, opacity: 0.5 }}
+        id="hero"
+      >
         <div className={styles.placeholder}>Hero</div>
         <div className={styles.placeholder} id="works">
           Projects
         </div>
       </header>
+      <section className={styles.placeholder}>spacer - invisible</section>
       <main style={{ position: "relative" }} id="main">
-        <section className={styles.placeholder}>spacer - invisible</section>
         <section className={styles.placeholder} id="about">
           About - 1 - presentation
         </section>
