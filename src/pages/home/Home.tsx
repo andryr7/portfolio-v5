f5df083f92e9193cf67b0fbfcf209e28b641ff16
unknown <andryratsimba@gmail.com>
2024-05-22 09:32:26 +0200

    add basic structure animation

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,28 +1,60 @@
 import { Link, useRoute } from "wouter";
 import styles from "./Home.module.css";
 
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
+
 export function Home() {
   const [match] = useRoute("/");
+  const container = useRef(null);
+
+  useGSAP(
+    () => {
+      // gsap code here...
+      gsap.to("#about", {
+        scrollTrigger: {
+          trigger: "#about",
+          start: "top top",
+          end: "bottom top",
+          pin: "#about",
+          markers: true,
+        },
+      });
+    }
+    // { scope: container }
+  );
 
   return (
     <>
-      <header className={styles.placeholder}>Hero</header>
-      <main>
-        <section className={styles.placeholder} id="works">
-          works
-          <Link href="/work/jacky">Test link</Link>
-        </section>
+      <header style={{ position: "absolute", zIndex: 20 }}>
+        <div className={styles.placeholder}>Hero</div>
+        <div className={styles.placeholder}>Projects</div>
+      </header>
+      <main style={{ position: "relative" }} id="test">
+        <section className={styles.placeholder}>spacer - invisible</section>
         <section className={styles.placeholder} id="about">
-          About - presentation
+          About - 1 - presentation
         </section>
-        <section className={styles.placeholder}>About - skills</section>
-        <section className={styles.placeholder}>About - techs</section>
+        <section className={styles.placeholder}>About - 2 - skills</section>
+        <section className={styles.placeholder}>About - 3 - techs</section>
         <section className={styles.placeholder} id="contact">
           Contact
         </section>
       </main>
-      <footer className={styles.placeholder} style={{ height: "50vh" }}>
-        Footer
+      <footer
+        className={styles.placeholder}
+        style={{
+          position: "relative",
+          zIndex: 40,
+          height: "50vh",
+          backgroundColor: "var(--color-main)",
+        }}
+      >
+        footer
       </footer>
     </>
   );
