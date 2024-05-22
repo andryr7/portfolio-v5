ae8445384d78b3505b02b823e5c6c162e1ba5772
unknown <andryratsimba@gmail.com>
2024-05-22 11:47:46 +0200

    add scroll progress and window resize animation

--- src/components/canvas/main/OverlayScene.tsx
+++ src/components/canvas/main/OverlayScene.tsx
@@ -1,9 +1,17 @@
+import { OrbitControls, Text } from "@react-three/drei";
+import { useThree } from "@react-three/fiber";
 import { useLenis } from "lenis/react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export function OverlayScene() {
   const [scrollProgress, setScrollProgress] = useState(0);
+  const { getCurrentViewport } = useThree((state) => state.viewport);
+  const [viewportDimensions, setViewportDimensions] = useState({
+    width: getCurrentViewport().width,
+    height: getCurrentViewport().height,
+  });
 
+  //Handling scroll animation
   useLenis((state) => {
     const newScrollProgress =
       state.animatedScroll /
@@ -11,12 +19,37 @@ export function OverlayScene() {
     setScrollProgress(Math.min(newScrollProgress, 1));
   });
 
+  //Handling window resize
+  useEffect(() => {
+    // Define the handleResize function
+    const handleResize = () => {
+      const viewport = getCurrentViewport();
+      setViewportDimensions({ width: viewport.width, height: viewport.height });
+    };
+
+    // Add event listener for resize
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup function to remove the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [getCurrentViewport]);
+
   return (
     <>
-      <mesh>
-        <boxGeometry />
-        <meshBasicMaterial color="red" />
-      </mesh>
+      <group>
+        <Text>Hello</Text>
+      </group>
+      <group>
+        <mesh>
+          <planeGeometry
+            args={[viewportDimensions.width, viewportDimensions.height]}
+          />
+          <meshBasicMaterial color="red" />
+        </mesh>
+      </group>
+      <OrbitControls />
     </>
   );
 }
