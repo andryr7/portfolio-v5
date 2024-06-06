e517b173b58cb46ed6bea5d14da27375439138ff
unknown <andryratsimba@gmail.com>
2024-06-06 16:51:39 +0200

    add contact section title

--- src/components/html/home/contact/ContactSection.tsx
+++ src/components/html/home/contact/ContactSection.tsx
@@ -1,23 +1,55 @@
 import { usePortfolioStore } from "@/handlers/usePortfolioStore";
-import { ContactLink } from "./ContactLink";
 import styles from "./ContactSection.module.css";
+import { useCallback, useState } from "react";
 
 export function ContactSection() {
+  const [emailWasCopied, setEmailWasCopied] = useState<boolean>(false);
   const setHoveredContactLink = usePortfolioStore(
     (state) => state.setHoveredContactLink
   );
 
+  const handleDesktopEmailClick = useCallback(() => {
+    navigator.clipboard.writeText("contact@andryratsimba.com");
+    setEmailWasCopied(true);
+    setTimeout(() => setEmailWasCopied(false), 5000);
+  }, []);
+
   return (
     <div className={styles.container}>
+      <div className={styles.sectionTitle}>contact</div>
       <div className={styles.linksContainerWrapper}>
         <div
           className={styles.linksContainer}
           onMouseLeave={() => setHoveredContactLink(null)}
         >
-          <div className={styles.captionContainer}>test</div>
-          <ContactLink index={0}>1</ContactLink>
-          <ContactLink index={1}>2</ContactLink>
-          <ContactLink index={2}>3</ContactLink>
+          <div className={styles.captionContainer}>
+            open for business - feel free to contact me
+          </div>
+          <a
+            className={styles.contactLink}
+            onMouseEnter={() => setHoveredContactLink(0)}
+            onClick={handleDesktopEmailClick}
+          >
+            {emailWasCopied ? "email was copied" : "-> email"}
+          </a>
+          <a
+            className={styles.contactLink}
+            onMouseEnter={() => setHoveredContactLink(1)}
+            target="_blank"
+            rel="noreferrer noopener"
+            href="https://www.linkedin.com/in/andryratsimba/"
+          >
+            {"-> "}linkedin
+          </a>
+          <a
+            className={styles.contactLink}
+            onMouseEnter={() => setHoveredContactLink(2)}
+            target="_blank"
+            rel="noreferrer noopener"
+            href="https://github.com/andryr7"
+          >
+            {"-> "}github
+          </a>
         </div>
       </div>
     </div>
