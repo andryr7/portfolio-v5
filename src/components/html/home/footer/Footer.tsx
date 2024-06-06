5b1dc3c2478a9435239535a0a587f2ebc219b92f
unknown <andryratsimba@gmail.com>
2024-06-06 11:17:51 +0200

    add footer animation

--- src/components/html/home/footer/Footer.tsx
+++ src/components/html/home/footer/Footer.tsx
@@ -1,9 +1,20 @@
+import { useLenis } from "lenis/react";
 import styles from "./Footer.module.css";
 
 export function Footer() {
+  const year = new Date().getFullYear();
+  const lenis = useLenis();
+
   return (
     <div className={styles.container}>
-      <span>©2024 - Andry Ratsimba</span>
+      <div className={styles.linksContainer}>
+        <a>data policy{" ->"}</a>
+        <a onClick={() => lenis?.scrollTo(0)}>{"<- "}back to the top</a>
+      </div>
+      <div className={styles.marqueeContainer}>
+        <span className={styles.marquee}>©{year} - Andry Ratsimba&nbsp; </span>
+        <span className={styles.marquee}>©{year} - Andry Ratsimba&nbsp; </span>
+      </div>
     </div>
   );
 }
