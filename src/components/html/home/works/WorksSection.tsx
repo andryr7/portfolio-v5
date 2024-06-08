2017ab24396647df3bcf62820e2e3d3ef39ecf23
unknown <andryratsimba@gmail.com>
2024-06-08 11:45:11 +0200

    add works hover transition

--- src/components/html/home/works/WorksSection.tsx
+++ src/components/html/home/works/WorksSection.tsx
@@ -4,6 +4,7 @@ import { Link } from "wouter";
 
 export function WorksSection() {
   const worksData = usePortfolioStore((state) => state.worksData);
+  const hoveredWorkIndex = usePortfolioStore((state) => state.hoveredWorkIndex);
   const setHoveredWorkIndex = usePortfolioStore(
     (state) => state.setHoveredWorkIndex
   );
@@ -13,6 +14,7 @@ export function WorksSection() {
       <ul
         className={styles.worksContainer}
         onMouseLeave={() => setHoveredWorkIndex(null)}
+        style={hoveredWorkIndex !== null ? { borderColor: "#0e0e0e11" } : {}}
       >
         {worksData.map((work, index) => (
           <Link
@@ -20,6 +22,13 @@ export function WorksSection() {
             className={styles.workLine}
             key={index}
             onMouseEnter={() => setHoveredWorkIndex(index)}
+            style={
+              hoveredWorkIndex !== null
+                ? hoveredWorkIndex !== index
+                  ? { opacity: 0.5, borderColor: "#0e0e0e11" }
+                  : { borderColor: "#0e0e0e11" }
+                : {}
+            }
           >
             <h3>{work.title}</h3>
             <span>{work.caption}</span>
