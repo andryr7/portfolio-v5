90eaf5cf7e7b65dd60d099515959130cb99a044d
unknown <andryratsimba@gmail.com>
2024-06-10 17:41:35 +0200

    small modifsé

--- src/components/html/home/about/technologies/Technologies.tsx
+++ src/components/html/home/about/technologies/Technologies.tsx
@@ -1,14 +1,44 @@
 import { View } from "@react-three/drei";
 import styles from "./Technologies.module.css";
 import { TechnologiesScene } from "@/components/canvas/home/technologies/TechnologiesScene";
+import { usePortfolioStore } from "@/handlers/usePortfolioStore";
 
 export function Technologies() {
+  const techs = usePortfolioStore((state) =>
+    state.techsData.filter((tech) => tech.showcased)
+  );
+
+  const selectedTechCubeId = usePortfolioStore(
+    (state) => state.selectedTechCubeId
+  );
+
+  const setSelectedTechCubeId = usePortfolioStore(
+    (state) => state.setSelectedTechCubeId
+  );
+
   return (
     <div className={styles.container}>
-      <h3 className={styles.title}>What I do</h3>
-      <View className={styles.view}>
-        <TechnologiesScene />
-      </View>
+      <h3 className={styles.title}>What I use</h3>
+      <div className={styles.viewContainer}>
+        <View
+          className={styles.view}
+          style={{ width: selectedTechCubeId === null ? "100%" : "70%" }}
+        >
+          <TechnologiesScene />
+        </View>
+        <div
+          className={styles.techDetailsWrapper}
+          style={{ right: selectedTechCubeId === null ? "-30%" : "0%" }}
+        >
+          <div className={styles.detailsContainer}>
+            <h4>name</h4>
+            <span>type</span>
+            <p>description</p>
+            <a>more info</a>
+          </div>
+          <button onClick={() => setSelectedTechCubeId(null)}>close</button>
+        </div>
+      </div>
     </div>
   );
 }
