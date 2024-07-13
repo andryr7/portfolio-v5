a929e939d6ace448eed33d54e5761ac558a52c05
unknown <andryratsimba@gmail.com>
2024-07-13 09:58:13 +0200

    small improvements

--- src/components/html/frame/Frame.tsx
+++ src/components/html/frame/Frame.tsx
@@ -3,11 +3,15 @@ import { Menu } from "./menu/Menu";
 import styles from "./Frame.module.css";
 import { Options } from "./options/Options";
 import { usePortfolioStore } from "@/handlers/usePortfolioStore";
+import { Link, useRoute } from "wouter";
+import { useLenis } from "lenis/react";
 
 export function Frame() {
+  const lenis = useLenis();
   const worksScrollProgress = usePortfolioStore(
     (state) => state.worksScrollProgress
   );
+  const [match] = useRoute("/");
 
   return (
     <>
@@ -15,12 +19,23 @@ export function Frame() {
         <div className={styles.container}>
           <div className={styles.interfaceContainer}>
             <Options />
-            <Menu />
+            {match && <Menu />}
+            {!match && (
+              <Link
+                href="/"
+                className={styles.homeLink}
+                onClick={() => lenis?.scrollTo(0)}
+              >
+                Back to homepage
+              </Link>
+            )}
           </div>
-          <div
-            className={styles.indicator}
-            style={{ opacity: worksScrollProgress === 0 ? 1 : 0 }}
-          >{`<- scroll or select ->`}</div>
+          {match && (
+            <div
+              className={styles.indicator}
+              style={{ opacity: worksScrollProgress === 0 ? 1 : 0 }}
+            >{`<- scroll or select ->`}</div>
+          )}
         </div>
       </BrowserView>
     </>
