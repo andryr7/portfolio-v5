2138a90351f70ea13464a00bea48caab67b34ba3
unknown <andryratsimba@gmail.com>
2024-08-10 09:29:47 +0200

    switched to online data fetching

---
+++ src/handlers/sanity.ts
@@ -0,0 +1,25 @@
+import { createClient } from "@sanity/client";
+
+export const client = createClient({
+  projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
+  dataset: import.meta.env.VITE_SANITY_DATASET,
+  useCdn: true, // set to `false` to bypass the edge cache
+  apiVersion: "2024-08-08", // use current date (YYYY-MM-DD) to target the latest API version
+});
+
+export async function getWorksData() {
+  const worksData = await client.fetch(
+    '*[_type == "work"]{..., usedTechs[]->{_id, name, type, url}} | order(order asc)'
+  );
+  return worksData;
+}
+
+export async function getTechsData() {
+  const techsData = await client.fetch('*[_type == "tech"]');
+  return techsData;
+}
+
+export async function getSkillsData() {
+  const skillsData = await client.fetch('*[_type == "skill"]');
+  return skillsData;
+}
--- src/handlers/useLoadData.ts
+++ src/handlers/useLoadData.ts
@@ -1,51 +1,59 @@
 import { useCallback, useEffect, useState } from "react";
 import { usePortfolioStore } from "./usePortfolioStore";
-import { useShallow } from "zustand/react/shallow";
+import { getSkillsData, getTechsData, getWorksData } from "./sanity";
 
 export function useLoadData() {
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  const { setWorksData, setSkillsData, setTechsData } = usePortfolioStore(
-    useShallow((state) => ({
-      setWorksData: state.setWorksData,
-      setSkillsData: state.setSkillsData,
-      setTechsData: state.setTechsData,
-    }))
-  );
 
-  const fetchData = useCallback(
-    async (
-      url: string,
-      setData: typeof setWorksData | typeof setSkillsData | typeof setTechsData
-    ) => {
-      const requestHeaders = {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      };
+  const setWorksData = usePortfolioStore((state) => state.setWorksData);
+  const setSkillsData = usePortfolioStore((state) => state.setSkillsData);
+  const setTechsData = usePortfolioStore((state) => state.setTechsData);
 
-      try {
-        const response = await fetch(url, {
-          headers: requestHeaders,
-        });
-        const data = await response.json();
-        setData(data.result);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    },
-    []
-  );
+  // const readData = useCallback(
+  //   async (
+  //     url: string,
+  //     setData: typeof setWorksData | typeof setSkillsData | typeof setTechsData
+  //   ) => {
+  //     const requestHeaders = {
+  //       "Content-Type": "application/json",
+  //       Accept: "application/json",
+  //     };
+
+  //     try {
+  //       const response = await fetch(url, {
+  //         headers: requestHeaders,
+  //       });
+  //       const data = await response.json();
+  //       setData(data.result);
+  //     } catch (error) {
+  //       console.error("Error fetching data:", error);
+  //     }
+  //   },
+  //   []
+  // );
+
+  const fetchData = useCallback(async () => {
+    try {
+      const worksData = await getWorksData();
+      setWorksData(worksData);
+
+      const skillsData = await getSkillsData();
+      setSkillsData(skillsData);
+
+      const techsData = await getTechsData();
+      setTechsData(techsData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }, [setWorksData, setSkillsData, setTechsData]);
 
   const getData = useCallback(async () => {
     try {
-      await Promise.all([
-        fetchData("/data/works.json", setWorksData),
-        fetchData("/data/skills.json", setSkillsData),
-        fetchData("/data/techs.json", setTechsData),
-      ]);
+      await fetchData();
     } finally {
       setIsLoading(false);
     }
-  }, [fetchData, setSkillsData, setWorksData, setTechsData]);
+  }, [fetchData]);
 
   useEffect(() => {
     getData();
