018bba5c7ebd5d000ca28c544dc205fc68eab12a
unknown <andryratsimba@gmail.com>
2024-09-19 19:30:21 +0200

    changed skills 3d shapes

--- src/components/canvas/desktop/skills/SkillsScene.tsx
+++ src/components/canvas/desktop/skills/SkillsScene.tsx
@@ -1,7 +1,12 @@
 import { usePortfolioStore } from "@/handlers/usePortfolioStore";
-import { Bounds, Box, Dodecahedron, Sphere } from "@react-three/drei";
+import {
+  Bounds,
+  Box,
+  MeshTransmissionMaterial,
+  Plane,
+  Sphere,
+} from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-import { easing } from "maath";
 import { useRef } from "react";
 import { Group } from "three";
 
@@ -21,76 +26,61 @@ export function SkillsScene() {
       groupRef.current.rotation.y += delta * 0.1;
       groupRef.current.rotation.z += delta * 0.1;
     }
-
-    if (sphereMaterialRef.current !== null) {
-      easing.damp(
-        sphereMaterialRef.current,
-        "opacity",
-        skillsScrollProgress <= 0.33 ? 1 : 0,
-        0.25,
-        delta
-      );
-    }
-
-    if (boxMaterialRef.current !== null) {
-      easing.damp(
-        boxMaterialRef.current,
-        "opacity",
-        skillsScrollProgress > 0.33 && skillsScrollProgress < 0.66 ? 1 : 0,
-        0.25,
-        delta
-      );
-    }
-
-    if (shapeMaterialRef.current !== null) {
-      easing.damp(
-        shapeMaterialRef.current,
-        "opacity",
-        skillsScrollProgress >= 0.66 ? 1 : 0,
-        0.25,
-        delta
-      );
-    }
   });
 
   return (
     <>
+      {/* <OrbitControls /> */}
       <group ref={groupRef}>
         <mesh>
-          <Sphere args={[1, 12, 2]}>
-            <meshBasicMaterial
-              color={colors.main}
-              wireframe
-              transparent
+          <Sphere>
+            <MeshTransmissionMaterial
+              chromaticAberration={1}
+              thickness={0.1}
+              transmission={1}
               ref={sphereMaterialRef}
-              visible={skillsScrollProgress <= 0.33}
             />
           </Sphere>
         </mesh>
         <mesh>
-          <Box args={[1.5, 1.5, 1.5]}>
+          <Box args={[1.15, 1.15, 1.15]}>
             <meshBasicMaterial
-              color={colors.main}
               wireframe
-              transparent
+              color={colors.main}
               ref={boxMaterialRef}
-              visible={
-                skillsScrollProgress > 0.33 && skillsScrollProgress < 0.66
-              }
+              visible={skillsScrollProgress > 0.33}
             />
           </Box>
         </mesh>
-        <mesh>
-          <Dodecahedron>
-            <meshBasicMaterial
-              color={colors.main}
-              wireframe
-              transparent
-              ref={shapeMaterialRef}
-              visible={skillsScrollProgress >= 0.66}
-            />
-          </Dodecahedron>
-        </mesh>
+        <group ref={shapeMaterialRef}>
+          <mesh>
+            <Plane args={[2, 2]}>
+              <meshBasicMaterial
+                wireframe
+                color={colors.main}
+                visible={skillsScrollProgress > 0.66}
+              />
+            </Plane>
+          </mesh>
+          <mesh rotation={[0, Math.PI / 2, 0]}>
+            <Plane args={[2, 2]}>
+              <meshBasicMaterial
+                wireframe
+                color={colors.main}
+                visible={skillsScrollProgress > 0.66}
+              />
+            </Plane>
+          </mesh>
+          <mesh rotation={[Math.PI / 2, 0, 0]}>
+            <Plane args={[2, 2]}>
+              <meshBasicMaterial
+                wireframe
+                color={colors.main}
+                visible={skillsScrollProgress > 0.66}
+              />
+            </Plane>
+          </mesh>
+        </group>
       </group>
 
       {/* Scene bounds */}
