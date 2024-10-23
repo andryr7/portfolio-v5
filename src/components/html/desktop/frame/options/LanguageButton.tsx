953417f14b0d6a82a9dd3980eb4d886e13b21900
unknown <andryratsimba@gmail.com>
2024-10-23 10:15:43 +0200

    changed option buttons behavior

--- src/components/html/desktop/frame/options/LanguageButton.tsx
+++ src/components/html/desktop/frame/options/LanguageButton.tsx
@@ -38,8 +38,20 @@ export function LanguageButton() {
 
   return (
     <div className={styles.container} onClick={handleClick}>
-      <div className={styles.languageButton}>{lang === "en" ? "en" : "fr"}</div>
-      <div className={styles.languageButton}>{lang === "en" ? "fr" : "en"}</div>
+      <div
+        className={`${styles.languageButton} ${
+          lang === "en" ? styles.active : styles.inactive
+        }`}
+        style={{ transform: `translateY(${lang === "en" ? "-100%" : "0%"})` }}
+      >
+        fr
+      </div>
+      <div
+        className={styles.languageButton}
+        style={{ transform: `translateY(${lang === "en" ? "0%" : "100%"})` }}
+      >
+        en
+      </div>
     </div>
   );
 }
